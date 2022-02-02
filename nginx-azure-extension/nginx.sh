0436fd0a5bf4c6a7296c65eab4d7b2d37ca3602b
Parveen Singh <81830224+psinghca@users.noreply.github.com>
2022-02-02 12:16:40 -0800

    Update nginx.sh

--- nginx-azure-extension/nginx.sh
+++ nginx-azure-extension/nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudoapt-get update -y && apt-get upgrade -y
 sudo apt-get install -y nginx
-sudo mkdir /var/www && sudo mkdir /var/www/html && echo "Congratulations! You have successfully installed NGINX on" $HOSTNAME "! using Azure VM Extension Command" | sudo tee -a /var/www/html/index.html
+echo "Congratulations! You have successfully installed NGINX on" $HOSTNAME "! using Azure VM Extension Command" | sudo tee -a /var/www/html/index.html
