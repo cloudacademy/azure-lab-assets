46a1f5041aa0810d5c2bc2287569225a6dffe72b
Parveen Singh <81830224+psinghca@users.noreply.github.com>
2022-02-09 13:56:52 -0800

    Create nginx-stress.sh

---
+++ nginx-azure-extension/nginx-stress.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudoapt-get update -y && apt-get upgrade -y
+sudo apt-get install -y nginx
+echo "Congratulations! You have successfully installed NGINX using Azure VM Extension Command" | sudo tee -a /var/www/html/index.html
+sudo apt-get update -y && sudo apt install stress -y
+nohup stress --cpu 1000 &
