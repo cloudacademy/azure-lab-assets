14c32cbf21dfb5e93f065f53a5601d585269981f
Parveen Singh <parveen.singh@cloudacademy.com>
2022-03-22 18:30:09 -0700

    Updating PS Domain Script

--- az-avd-lab-assets/dsc-domain/CreateAD.ps1
+++ az-avd-lab-assets/dsc-domain/CreateAD.ps1
@@ -1,3 +1,4 @@
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 $theSecureString = ConvertTo-SecureString "1Cloud_Academy_Labs!" -AsPlainText -Force
 Install-ADDSForest -DomainName "cloudacademylabs.com" -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "7" -DomainNetbiosName "calabs" -ForestMode "7" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword $theSecureString
+Restart-Computer -Force
